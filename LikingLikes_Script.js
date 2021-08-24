3876273ea49deadcca1dfb01cf051adaa7ae8a3a
Luke Hufnagle <leh17068@gmail.com>
2021-08-24 17:15:50 -0400

    Optimized js code

--- LikingLikes_Script.js
+++ LikingLikes_Script.js
@@ -1,28 +1,29 @@
-var count = 0;
+var count = [0,0,0]
+var select = [document.querySelector("#count"), document.querySelector("#count2"), document.querySelector("#count3")]
 
-var countElement = document.querySelector("#count");
+// var countElement = ;
 
-function add1(){
-    count++;
-    countElement.innerText = count + " like(s)";
-    console.log(count)
+function add(value){
+    count[value]++;
+    select[value].innerText = count[value] + " like(s)";
+    // console.log(count)
 }
 
-var startCount = 0;
+// var startCount = 0;
 
-var countElement2 = document.getElementById("count2");
+// var countElement2 = document.getElementById("count2");
 
-function add1two(){
-    startCount++;
-    countElement2.innerText = startCount + " like(s)"
-    console.log(startCount);
-}
+// function add1two(){
+//     startCount++;
+//     countElement2.innerText = startCount + " like(s)"
+//     console.log(startCount);
+// }
 
-var count3 = 0;
+// var count3 = 0;
 
-var countElement3 = document.querySelector("#count3");
+// var countElement3 = document.querySelector("#count3");
 
-function add1three(){
-    count3++;
-    countElement3.innerText = count3 + " like(s)"
-}
+// function add1three(){
+//     count3++;
+//     countElement3.innerText = count3 + " like(s)"
+// }
